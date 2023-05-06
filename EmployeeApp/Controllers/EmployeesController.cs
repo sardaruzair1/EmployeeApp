57dda27c0b06cfd6724de84976db9cebec83dcba
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-05-06 23:25:27 +0500

    Creat Done

---
+++ EmployeeApp/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeApp.Data;
+using EmployeeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDataDbContext employeeDbContext;
+
+        public EmployeesController(EmployeeDataDbContext employeeDbContext)
+        {
+            this.employeeDbContext = employeeDbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeViewRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeViewRequest.Name,
+                City = addEmployeeViewRequest.City,
+                State = addEmployeeViewRequest.State,
+                Salary = addEmployeeViewRequest.Salary,
+            };
+            await employeeDbContext.Employees.AddAsync(employee);
+            await employeeDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+    }
+}
