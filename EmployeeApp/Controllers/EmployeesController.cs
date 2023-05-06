45b130e53ee27fa33560789fe4bc4ccef5ad471e
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-05-07 01:38:43 +0500

    full crud completed in .net

--- EmployeeApp/Controllers/EmployeesController.cs
+++ EmployeeApp/Controllers/EmployeesController.cs
@@ -39,5 +39,52 @@ namespace EmployeeApp.Controllers
             await employeeDbContext.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await employeeDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if(employee != null)
+            {
+                var ViewModel = new EmployeeUpdate()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    City = employee.City,
+                    State = employee.State,
+                    Salary = employee.Salary,
+                };
+                return await Task.Run(()=> View("View",ViewModel));
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(EmployeeUpdate model)
+        {
+            var employee = await employeeDbContext.Employees.FindAsync(model.Id);
+            if(employee != null)
+            {
+                employee.Name = model.Name;
+                employee.City = model.City;
+                employee.State = model.State;
+                employee.Salary = model.Salary;
+                
+                await employeeDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(EmployeeUpdate model)
+        {
+            var employee = await employeeDbContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                employeeDbContext.Employees.Remove(employee);
+                await employeeDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ EmployeeApp/Models/EmployeeUpdate.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeApp.Models
+{
+    public class EmployeeUpdate
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public long Salary { get; set; }
+    }
+}
